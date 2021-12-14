9da9d4eac8f68c437b69a42e0d00564ee3f52962
Juan Escalante <jcec007@gmail.com>
2021-12-14 15:29:16 -0500

    +Update: improve Slider layouts

--- Resources/views/frontend/components/slider/owl/layouts/slider-owl-layout-3/index.blade.php
+++ Resources/views/frontend/components/slider/owl/layouts/slider-owl-layout-3/index.blade.php
@@ -40,24 +40,4 @@
     @endif
   @endforeach
 </div>
-@section('scripts-owl')
-  @parent
-  <script>
-    $(document).ready(function () {
-      $('#{{ $slider->system_name }}').owlCarousel({
-        items: 1,
-        dots: {!! $dots ? 'true' : 'false' !!},
-        loop: {!! $loop ? 'true' : 'false' !!},
-        lazyLoad: true,
-        margin: {!! $margin !!},
-        nav: {!! $nav ? 'true' : 'false' !!},
-        autoplay: {!! $autoplay ? 'true' : 'false' !!},
-        autoplayHoverPause: {!! $autoplayHoverPause ? 'true' : 'false' !!},
-        responsiveClass: {!! $responsiveClass ? 'true' : 'false' !!},
-        responsive: {!! $responsive!!},
-        autoplayTimeout: {{$autoplayTimeout}},
-        {!! !empty($navText) ? 'navText: '.$navText."," : "" !!}
-      });
-    });
-  </script>
-@stop
+@include("slider::frontend.components.slider.owl.script")
---
+++ Resources/views/frontend/components/slider/owl/script.blade.php
@@ -0,0 +1,21 @@
+@section('scripts-owl')
+  @parent
+  <script>
+    $(document).ready(function () {
+      $('#{{ $slider->system_name }}').owlCarousel({
+        items: 1,
+        dots: {!! $dots ? 'true' : 'false' !!},
+        loop: {!! $loopOwl ? 'true' : 'false' !!},
+        lazyLoad: true,
+        margin: {!! $margin !!},
+        nav: {!! $nav ? 'true' : 'false' !!},
+        autoplay: {!! $autoplay ? 'true' : 'false' !!},
+        autoplayHoverPause: {!! $autoplayHoverPause ? 'true' : 'false' !!},
+        responsiveClass: {!! $responsiveClass ? 'true' : 'false' !!},
+        responsive: {!! $responsive!!},
+        autoplayTimeout: {{$autoplayTimeout}},
+        {!! !empty($navText) ? 'navText: '.$navText."," : "" !!}
+      });
+    });
+  </script>
+@stop
