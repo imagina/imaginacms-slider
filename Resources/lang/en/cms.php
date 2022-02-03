a1003fb359c259e6ba4392d8ef9b5bc5cfd323d8
Michael <msolano@imaginacolombia.com>
2022-02-03 12:36:51 -0500

    Add: CMS Translations

---
+++ Resources/lang/en/cms.php
@@ -0,0 +1,12 @@
+<?php
+return [
+  "newSlider"  => 'New Slider',
+  "updateSlider"  => 'Update Slider',
+  "newSlide"  => 'New Item',
+  "updateSlide"  => 'Update Item',
+  "label"  => [
+    "codeAds"   => 'Code ADS',
+    "showAsPopup"  => 'Show as Popup',
+    "buttonText" => 'Button Text',
+  ]
+];
---
+++ Resources/lang/es/cms.php
@@ -0,0 +1,13 @@
+<?php
+return [
+  "newSlider"  => 'Nuevo Slider',
+  "updateSlider"  => 'Actualizar Slider',
+  "createSlider"  => 'Crear Slider',
+  "newSlide"  => 'Nuevo Elemento',
+  "updateSlide"  => 'Actualizar Elemento',
+  "label"  => [
+    "codeAds"   => 'Codigo ADS',
+    "showAsPopup"  => 'Mostrar como ventana emergente',
+    "buttonText" => 'Texto del Botón',
+  ]
+];
