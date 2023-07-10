9e273e64ed5be6cf6fde2164811f66846b50083f
William Verde <williamverde10@gmail.com>
2023-07-10 15:16:55 -0400

    Update: fix variable name

--- Services/SliderContentAi.php
+++ Services/SliderContentAi.php
@@ -21,8 +21,8 @@ class SliderContentAi
     $prompt = "Contenido llamativo para slides rotatorios de una pagina WEB con los siguientes atributos ";
     //Instance attributes
     $prompt .= $this->aiService->getStandardPrompts(["title", "summary"]) .
-      "custom_html: Que contenga entre 200 y 300 palabras, el texto sea en formato HTML, {$this->iaService->translatablePrompt} " .
-      "caption: texto corto de maximo 2 palabras que pueda ser usado para un boton el slide, {$this->iaService->translatablePrompt} ";
+      "custom_html: Que contenga entre 200 y 300 palabras, el texto sea en formato HTML, {$this->aiService->translatablePrompt} " .
+      "caption: texto corto de maximo 2 palabras que pueda ser usado para un boton el slide, {$this->aiService->translatablePrompt} ";
     //Call IA Service
     $response = $this->aiService->getContent($prompt, $quantity);
     //Return response
