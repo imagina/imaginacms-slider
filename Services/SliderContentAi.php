5333c23eb5dd0e71d7163516cdeaa09993093152
William Verde <williamverde10@gmail.com>
2023-07-17 10:21:30 -0400

    Added: Base to slider content ai

--- Services/SliderContentAi.php
+++ Services/SliderContentAi.php
@@ -10,10 +10,19 @@ class SliderContentAi
 {
   public $aiService;
   private $log = "Slider: Services|SliderContentAi|";
+  private $sliderRepository;
+  private $slideRepository;
+  private $maxAttempts;
+  private $slideQuantity;
+  private $systemName = "principal";
 
-  function __construct()
+  function __construct($maxAttempts = 3, $slideQuantity = 2)
   {
     $this->aiService = new AiService();
+    $this->maxAttempts = $maxAttempts;
+    $this->slideQuantity = $slideQuantity;
+    $this->sliderRepository = app("Modules\Slider\Repositories\SliderApiRepository");
+    $this->slideRepository = app("Modules\Slider\Repositories\SlideApiRepository");
   }
 
   public function getSlides($quantity = 2)
@@ -32,4 +41,83 @@ class SliderContentAi
     //Return response
     return $response;
   }
+
+   /**
+  * Principal
+  */
+  public function startProcesses()
+  {
+
+    \Log::info($this->log."startProcesses");
+
+    $newData = $this->getNewData();
+    if(!is_null($newData)){
+
+      //Get principal slider
+      $params = ["filter" => ["field" => "system_name"]];
+      $slider = $this->sliderRepository->getItem($this->systemName,json_decode(json_encode($params)));
+
+      $this->deleteOldSlides($slider);
+      $this->createSlides($newData,$slider);
+      
+    }
+
+  }
+  /**
+  * Get the New Data
+  */
+  public function getNewData()
+  {
+    
+    $newData = null;
+
+    $attempts = 0;
+    do {
+      \Log::info($this->log."getNewData|Attempt:".($attempts+1)."/Max:".$this->maxAttempts);
+      $newData = $this->getSlides($this->slideQuantity);
+  
+      if(is_null($newData)){
+        $attempts++;
+      }else{
+        if(isset($newData[0]['es']) && isset($newData[0]['en']))
+          break;
+        else
+          $attempts++;
+      }
+    }while($attempts < $this->maxAttempts);
+
+    return $newData;
+  }
+
+  /**
+  * Slides to Create
+  */
+  public function createSlides($slides,$slider)
+  {
+
+    \Log::info($this->log."createSlides");
+    foreach ($slides as $key => $slide) {
+
+      $slide['slider_id'] = $slider->id;
+      $slide['type'] = NULL;
+
+      $result = $this->slideRepository->create($slide);
+
+      //TODO
+      //Proceso to create image
+
+    }
+  }
+
+  /**
+   * Delete old Slides
+   */
+  public function deleteOldSlides($slider)
+  {
+
+    \Log::info($this->log."deleteOldSlides");
+    $slider->slides()->delete();
+
+  }
+
 }
