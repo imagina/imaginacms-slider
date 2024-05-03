cbb8b87c3f5fdba2531ae98e944e67c560385a5b
Cristian Marin <marin101998@gmail.com>
2024-05-03 11:40:14 -0500

    +update | v10.x 03-05-24 last version v8.x

--- Resources/views/frontend/components/slider/owl/layouts/slider-owl-layout-5/index.blade.php
+++ Resources/views/frontend/components/slider/owl/layouts/slider-owl-layout-5/index.blade.php
@@ -1,40 +1,38 @@
-@if(count($slider->slides)==1)
-        @php
-                $mouseDrag=false;
-                $touchDrag=false;
-                $dots=false;
-                $nav=false;
-                $navText=[];
-        @endphp
+@if(count($slides)==1)
+  @php
+    $mouseDrag=false;
+    $touchDrag=false;
+    $dots=false;
+    $nav=false;
+    $navText=[];
+  @endphp
 @endif
 <div id="{{ $slider->system_name }}Slider">
-<div id="{{ $slider->system_name }}"
-     class="owl-carousel owl-theme owl-slider-layout-5 {{ $nav ? ' owl-with-nav carousel-nav-position-'.$navPosition : '' }} {{ $dots ? ' owl-with-dots carousel-indicators-position-'.$dotsPosition.' carousel-indicators-style-'. $dotsStyle: '' }} position-relative">
-    @foreach($slider->slides as $index => $slide)
-      @if($isMobile)
-        @if(isset($slide->responsive) && $slide->responsive != 2)
-          @if(isset($slide->code_ads) && !is_null($slide->code_ads))
-            <div class="banner-{{$slide->id}} py-3">
-              {!! $slide->code_ads !!}
-            </div>
-          @else
-            <div class="slide">
-              @php $itemComponentAttributes += ['itemComponentTarget' => $slide->target] @endphp
-              @if(!empty($itemComponentAttributes['viewMoreButtonLabel']))
-                @php
-                  $itemComponentAttributes['viewMoreButtonLabel'] = $slide->caption ?? trans('isite::common.menu.viewMore');
-                @endphp
-              @else
-                @php
-                  $itemComponentAttributes += ['viewMoreButtonLabel' => $slide->caption ?? trans('isite::common.menu.viewMore') ];
-                @endphp
-              @endif
-              @include("isite::frontend.partials.item",["item" => $slide, "itemLayout" => $itemComponentAttributes['layout'],"itemComponentAttributes" => $itemComponentAttributes])
-            </div>
-          @endif
+  <div id="{{ $slider->system_name }}"
+       class="owl-carousel owl-theme owl-slider-layout-5 {{ $nav ? ' owl-with-nav carousel-nav-position-'.$navPosition : '' }} {{ $dots ? ' owl-with-dots carousel-indicators-position-'.$dotsPosition.' carousel-indicators-style-'. $dotsStyle: '' }} position-relative">
+    @foreach($slides as $index => $slide)
+      @if($slide->responsive != 2)
+        @if(isset($slide->code_ads) && !is_null($slide->code_ads))
+          <div class="banner-{{$slide->id}} py-3">
+            {!! $slide->code_ads !!}
+          </div>
+        @else
+          <div class="slide">
+            @php $itemComponentAttributes += ['itemComponentTarget' => $slide->target] @endphp
+            @if(!empty($itemComponentAttributes['viewMoreButtonLabel']))
+              @php
+                $itemComponentAttributes['viewMoreButtonLabel'] = $slide->caption ?? trans('isite::common.menu.viewMore');
+              @endphp
+            @else
+              @php
+                $itemComponentAttributes += ['viewMoreButtonLabel' => $slide->caption ?? trans('isite::common.menu.viewMore') ];
+              @endphp
+            @endif
+            @include("isite::frontend.partials.item",["item" => $slide, "itemLayout" => $itemComponentAttributes['layout'],"itemComponentAttributes" => $itemComponentAttributes])
+          </div>
         @endif
       @endif
-      @if(isset($slide->responsive) && $slide->responsive != 3)
+      @if($slide->responsive != 3)
         @if(isset($slide->code_ads) && !is_null($slide->code_ads))
           <div class="banner-{{$slide->id}} py-3">
             {!! $slide->code_ads !!}
@@ -56,7 +54,7 @@
         @endif
       @endif
     @endforeach
-</div>
+  </div>
 </div>
 @include("slider::frontend.components.slider.owl.script")
 @include("slider::frontend.components.slider.owl.layouts.slider-owl-layout-5.style")
--- View/Components/Slider/Owl.php
+++ View/Components/Slider/Owl.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace Modules\Slider\View\Components\Slider;
-
 use Illuminate\View\Component;
 
 class Owl extends Component
@@ -43,7 +42,6 @@ class Owl extends Component
   public $navLateralLeftRight;
   public $navLateralTop;
   public $dotsBottom;
-  public $isMobile;
 
   /**
    * Create a new component instance.
@@ -52,83 +50,86 @@ class Owl extends Component
    */
   public function __construct($id, $layout = 'slider-owl-layout-1', $height = '500px', $autoplay = true, $margin = 0,
                               $autoplayHoverPause = true, $loop = true, $dots = true, $dotsPosition = 'center',
-                              $dotsStyle = 'line', $nav = true, $navText = '', $autoplayTimeout = 10000, $imgObjectFit = 'cover',
+                              $dotsStyle = 'line', $nav = true, $navText = "", $autoplayTimeout = 10000, $imgObjectFit = "cover",
                               $responsiveClass = false, $responsive = null, $orderClasses = [], $withViewMoreButton = true,
-                              $container = 'container', $stagePadding = 0, $view = null, $itemComponentAttributes = [],
+                              $container="container", $stagePadding = 0, $view = null, $itemComponentAttributes = [],
                               $itemComponentNamespace = null, $itemComponent = null, $navPosition = 'lateral',
                               $mouseDrag = true, $touchDrag = true, $navLateralTop = 50, $navLateralLeftRight = '15px',
                               $dotsStyleColor = '#fff', $dotsBottom = 0
-  ) {
-        $this->id = $id;
-        $this->layout = $layout ?? 'slider-owl-layout-1';
-        $this->height = $height ?? '500px';
-        $this->margin = $margin ?? 0;
-        $this->dots = $dots ?? true;
-        $this->dotsPosition = $dotsPosition ?? 'center';
-        $this->dotsStyle = $dotsStyle ?? 'line';
-        $this->dotsStyleColor = $dotsStyleColor;
-        $this->nav = $nav ?? true;
-        $this->navText = json_encode($navText);
-        $this->loopOwl = $loop ?? true;
-        $this->autoplay = $autoplay ?? true;
-        $this->autoplayHoverPause = $autoplayHoverPause ?? true;
-        $this->autoplayTimeout = $autoplayTimeout ?? 10000;
-        $this->imgObjectFit = $imgObjectFit ?? 'cover';
-        $this->responsive = json_encode($responsive ?? [0 => ['items' => 1]]);
-        $this->responsiveClass = $responsiveClass;
-        $this->orderClasses = ! empty($orderClasses) ? $orderClasses : ['photo' => 'order-0', 'content' => 'order-1'];
-        [$this->editLink, $this->tooltipEditLink] = getEditLink('Modules\Slider\Repositories\SlideRepository');
-        $this->withViewMoreButton = $withViewMoreButton;
-        $this->stagePadding = $stagePadding;
-        $this->container = $container;
-        $this->view = $view ?? "slider::frontend.components.slider.owl.layouts.{$this->layout}.index";
-        $this->getItem();
-        $this->itemComponent = $itemComponent ?? 'isite::item-list';
-        $this->itemComponentNamespace = $itemComponentNamespace ?? "Modules\Isite\View\Components\ItemList";
-        $this->itemComponentAttributes = count($itemComponentAttributes) ? $itemComponentAttributes : config('asgard.slider.config.indexItemListAttributes');
-        $this->navPosition = $navPosition ?? 'lateral';
-        $this->mouseDrag = $mouseDrag;
-        $this->touchDrag = $touchDrag;
-        $this->navLateralLeftRight = $navLateralLeftRight;
-        $this->navLateralTop = explode(',', $navLateralTop);
-        $this->dotsBottom = $dotsBottom;
-        $this->isMobile = isMobileDevice();
+  )
+  {
+    $this->id = $id;
+    $this->layout = $layout ?? 'slider-owl-layout-1';
+    $this->height = $height ?? '500px';
+    $this->margin = $margin ?? 0;
+    $this->dots = $dots ?? true;
+    $this->dotsPosition = $dotsPosition ?? 'center';
+    $this->dotsStyle = $dotsStyle ?? 'line';
+    $this->dotsStyleColor = $dotsStyleColor;
+    $this->nav = $nav ?? true;
+    $this->navText = json_encode($navText);
+    $this->loopOwl = $loop ?? true;
+    $this->autoplay = $autoplay ?? true;
+    $this->autoplayHoverPause = $autoplayHoverPause ?? true;
+    $this->autoplayTimeout = $autoplayTimeout ?? 10000;
+    $this->imgObjectFit = $imgObjectFit ?? "cover";
+    $this->responsive = json_encode($responsive ?? [0 => ["items" => 1]]);
+    $this->responsiveClass = $responsiveClass;
+    $this->orderClasses = !empty($orderClasses) ? $orderClasses : ["photo" => "order-0", "content" => "order-1"];
+    list($this->editLink, $this->tooltipEditLink) = getEditLink('Modules\Slider\Repositories\SlideRepository');
+    $this->withViewMoreButton = $withViewMoreButton;
+    $this->stagePadding = $stagePadding;
+    $this->container = $container;
+    $this->view = $view ?? "slider::frontend.components.slider.owl.layouts.{$this->layout}.index";
+    $this->getItem();
+    $this->itemComponent = $itemComponent ?? "isite::item-list";
+    $this->itemComponentNamespace =  $itemComponentNamespace ?? "Modules\Isite\View\Components\ItemList";
+    $this->itemComponentAttributes = count($itemComponentAttributes) ? $itemComponentAttributes : config('asgard.slider.config.indexItemListAttributes');
+    $this->navPosition = $navPosition ?? 'lateral';
+    $this->mouseDrag = $mouseDrag;
+    $this->touchDrag = $touchDrag;
+    $this->navLateralLeftRight = $navLateralLeftRight;
+    $this->navLateralTop = explode(",",$navLateralTop);
+    $this->dotsBottom = $dotsBottom;
+  }
+
+
+  public function getItem()
+  {
+    $params = [
+      'filter' => [
+        'field' => 'system_name',
+      ]
+    ];
+
+    $this->slider = app('Modules\\Slider\\Repositories\\SliderRepository')->getItem($this->id, json_decode(json_encode($params)));
+    if (!$this->slider) {
+      $params['filter']['field'] = 'id';
+      $this->slider = app('Modules\\Slider\\Repositories\\SliderRepository')->getItem($this->id, json_decode(json_encode($params)));
+
     }
 
-    public function getItem()
-    {
-        $params = [
-            'filter' => [
-                'field' => 'id',
-            ],
-        ];
+    $params = [
+      'filter' => [
+        'sliderId' => $this->slider->id ?? null,
+      ],
+      'include' => ['files','translations']
+    ];
 
-        $this->slider = app('Modules\\Slider\\Repositories\\SliderApiRepository')->getItem($this->id, json_decode(json_encode($params)));
-        if (! $this->slider) {
-            $params['filter']['field'] = 'system_name';
-            $this->slider = app('Modules\\Slider\\Repositories\\SliderApiRepository')->getItem($this->id, json_decode(json_encode($params)));
-        }
+    $this->slides = app('Modules\\Slider\\Repositories\\SlideRepository')->getItemsBy(json_decode(json_encode($params)));
 
-        $params = [
-            'filter' => [
-                'sliderId' => $this->slider->id ?? null,
-            ],
-        ];
 
-        $this->slides = app('Modules\\Slider\\Repositories\\SlideApiRepository')->getItemsBy(json_decode(json_encode($params)));
-    }
+  }
 
-    /**
-     * Get the view / contents that represent the component.
-     *
-     * @return \Illuminate\Contracts\View\View|string
-     */
-    public function render()
-    {
-        if (! isset($this->slider->id)) {
-            return view('slider::frontend.components.slider.owl.invalid-slider');
-        }
-
-        return view($this->view);
-    }
+  /**
+   * Get the view / contents that represent the component.
+   *
+   * @return \Illuminate\Contracts\View\View|string
+   */
+  public function render()
+  {
+    if(!isset($this->slider->id))
+      return view("slider::frontend.components.slider.owl.invalid-slider");
+    return view($this->view);
+  }
 }
