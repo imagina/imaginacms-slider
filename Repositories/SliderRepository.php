3a4eea5ff73e0c5f2e2914cbc257b953756bcdd9
Cristian Marin <marin101998@gmail.com>
2024-06-26 16:59:32 -0500

    + fix | v8 26-06-24 change and delete function find by system name

--- Repositories/SliderRepository.php
+++ Repositories/SliderRepository.php
@@ -6,8 +6,5 @@ use Modules\Core\Icrud\Repositories\BaseCrudRepository;
 
 interface SliderRepository extends BaseCrudRepository
 {
-
-    public function findBySystemName($systemName);
-
     public function countAll();
 }
